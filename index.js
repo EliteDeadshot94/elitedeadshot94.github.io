83f50bec54c52c60aa8d9f450648d973a668848d
EliteDeadshot94 <kjww200819@gmail.com>
2025-09-16 15:53:00 -0500

    update index.html and style.css to fix navigation bar styling

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-document.getElementById("myH1").textContent = "Calculator";
+document.getElementById("myH3") && (document.getElementById("myH3").textContent = "This is The Home Page");
+document.getElementById("myP") && (document.getElementById("myP").textContent = "Welcome to my website use the navagation bar to access the Calculator");
+document.getElementById("myH1") && (document.getElementById("myH1").textContent = "Calculator");
 
 const display = document.getElementById('display');
 
